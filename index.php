63b0aa5c7ba5e1133450436b2e25387d6bef5948
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-05-13 18:06:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+$values = [2,3,1];
+
+$outPut = [];
+
+
+for($i=0;$i<count($values);$i++){
+    if($values[$i] >= $values[$i + 1]){
+        array_push($outPut,$values[$i]);
+    }else{
+        $a = $values[$i];
+        $b = $values[$i +1];
+        $values[$i] = $b;
+        $values[$i + 1] = $a;
+
+    }
+    array_push($outPut,$values);
+}
+print_r($outPut);
